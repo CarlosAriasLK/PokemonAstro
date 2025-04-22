406a2c13742bded1e0d84fef307576f5e0d7c226
Carlos Arias <ceau922@gmail.com>
2025-04-22 11:38:11 -0500

    add(pages): islands

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -6,27 +6,23 @@ export const Counter = () => {
 
     const [ counter, setCounter ] = createSignal(10)
 
-
-
-
-
     return (
         <>
             <h1 class="text-5xl">Counter</h1>
             <h3 class="text-4xl">Value: { counter() } </h3>
 
             <button 
-                onClick={ () => setCounter( counter() + 1 ) }
-                class="bg-blue-500 p-3 mr-2 mt-3 rounded cursor-pointer" 
-            >
-                +1
-            </button>
-
-            <button 
                 onClick={ () => setCounter( counter() - 1 ) }
                 class="bg-blue-500 p-3 mr-2 mt-3 rounded cursor-pointer" 
             >
                 -1
+            </button>
+
+            <button 
+                onClick={ () => setCounter( counter() + 1 ) }
+                class="bg-blue-500 p-3 mr-2 mt-3 rounded cursor-pointer" 
+            >
+                +1
             </button>
         </>
     )
