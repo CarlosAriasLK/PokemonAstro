cf510e3d222b5531ede311a388a13c6b4e63755e
Carlos Arias <ceau922@gmail.com>
2025-04-22 12:18:09 -0500

    add(components): Favorite pokemon list added

---
+++ src/components/pokemons/FavoritePokemons.tsx
@@ -0,0 +1,29 @@
+import type { FavoritePokemon } from "@interfaces/favorite-pokemon";
+import { createSignal, For } from "solid-js";
+
+
+
+const getLocalStoragePokemons = (): FavoritePokemon[] => {
+    const favoritePokemons = JSON.parse(
+        localStorage.getItem('favorites') ?? '[]'
+    )
+
+    return favoritePokemons;
+}
+
+export const FavoritePokemons = () => {
+
+  const [ pokemons, setPokemons ] = createSignal( getLocalStoragePokemons() );
+
+  return (
+    <div class="grid gridcols2 sm:grid-cols-4">
+
+      <For each={ pokemons() }>
+        {
+          pokemon => <h1>{ pokemon.name }</h1>
+        }
+      </For>
+
+    </div>
+  )
+}
